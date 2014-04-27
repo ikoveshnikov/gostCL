a214a77db6b701a00aaefc216cedc3fe91ea332b
JackMonterey <JackMonterey91@gmail.com>
2014-04-27 20:54:43 +0600

    added libgostCL api

--- libgostCL/libgostcl.h
+++ libgostCL/libgostcl.h
@@ -2,12 +2,54 @@
 #define LIBGOSTCL_H
 
 #include "libgostcl_global.h"
+#include <list>
+#include <string>
+
+namespace GostCL {
+
 
 class LIBGOSTCLSHARED_EXPORT LibGostCL
 {
 
 public:
+    enum ModeOfOperation
+    {
+        ECB = 0,
+        CBC = 1,
+        OFB = 2,
+        CFB = 3
+    };
+
+
     LibGostCL();
-};
+    ~LibGostCL();
+
+    bool SetSBoxes ();
+
+    bool SetKey ();
+
+    bool Encrypt (const std::list * const data,
+                  ModeOfOperation mode = ECB,
+                  int threads = 1,
+                  int theadSize = -1); // in megabytes
+
+    bool Decrypt (const std::list * const data,
+                  ModeOfOperation mode = ECB,
+                  int threads = 1,
+                  int theadSize = -1);
+
+    bool GetResult (std::list * data) const;
+
+    std::string GetErrorString ();
+
+private:
+    bool errorHappen;
+    int errorCode;
+
+
+}; // class LibGostCL
+
+
+} // namespace GostCL
 
 #endif // LIBGOSTCL_H
