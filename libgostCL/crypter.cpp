db9969269ebfb187b276eecd625417be190f535a
JackMonterey <JackMonterey91@gmail.com>
2014-05-01 20:20:41 +0600

    Added itinial crypter implementetion

--- libgostCL/crypter.cpp
+++ libgostCL/crypter.cpp
@@ -1,5 +1,70 @@
 #include "crypter.h"
+#include "settingsholder.h"
 
 Crypter::Crypter()
+    : operationMode (LibGostCL::ECB)
+    , threadsCount (1)
+    , threadsSize (-1)
+    , errorCode (0)
 {
 }
+
+Crypter::~Crypter()
+{
+
+}
+
+void Crypter::SetModeOfOperation (LibGostCL::ModeOfOperation mode = LibGostCL::ECB)
+{
+    operationMode = mode;
+}
+
+void Crypter::SetThreadsCount (int threads)
+{
+    threadsCount = threads;
+}
+
+void Crypter::SetThreadsSize (int megabytes)
+{
+    threadsSize = megabytes;
+}
+
+int Crypter::GetErrorCode ()
+{
+    return errorCode;
+}
+
+bool Crypter::Encrypt (const std::list <unsigned char> * const data,
+                       const std::list <unsigned char> * result)
+{
+    std::string key = SettingsHolder::Instance().GetKey();
+
+    const std::vector <std::vector <int> > sboxes =
+            SettingsHolder::Instance().GetSBoxes();
+
+    if (0 == key.size())
+    {
+        errorCode = 2;
+        return false;
+    }
+    return true;
+}
+
+bool Crypter::Decrypt (const std::list <unsigned char> * const data,
+                       const std::list <unsigned char> * result)
+{
+    std::string key = SettingsHolder::Instance().GetKey();
+
+    const std::vector <std::vector <int> > sboxes =
+            SettingsHolder::Instance().GetSBoxes();
+
+    if (0 == key.size())
+    {
+        errorCode = 2;
+        return false;
+    }
+
+    return true;
+}
+
+
--- libgostCL/crypter.h
+++ libgostCL/crypter.h
@@ -1,12 +1,34 @@
 #ifndef CRYPTER_H
 #define CRYPTER_H
 
+#include "libgostcl.h"
+
 class Crypter
 {
 public:
     Crypter();
     ~Crypter();
 
+    void SetModeOfOperation (LibGostCL::ModeOfOperation mode = LibGostCL::ECB);
+
+    void SetThreadsCount (int threads);
+
+    void SetThreadsSize (int megabytes);
+
+    bool Encrypt (const std::list <unsigned char> * const data,
+                  const std::list <unsigned char> * result);
+
+    bool Decrypt (const std::list <unsigned char> * const data,
+                  const std::list <unsigned char> * result);
+
+    int GetErrorCode ();
+
+private:
+    LibGostCL::ModeOfOperation operationMode;
+
+    int threadsCount;
+    int threadsSize;
+    int errorCode;
 };
 
 #endif // CRYPTER_H
