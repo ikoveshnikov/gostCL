ac57f1d63d032e57967f9c9bbca54cd8e542be82
JackMonterey <JackMonterey91@gmail.com>
2014-05-07 00:02:19 +0600

    Added command line parser

--- gostCL-cli/main.cpp
+++ gostCL-cli/main.cpp
@@ -1,5 +1,7 @@
 #include "gostclcrypter.h"
 #include <iostream>
+#include <vector>
+#include <string>
 
 void PrintHelp (const char * programName)
 {
@@ -39,13 +41,209 @@ void PrintHelp (const char * programName)
 }
 
 
-
-
-
 int main(int argc, char *argv[])
 {
+    if ((argc > 10) || (argc <= 1))
+    {
+        PrintHelp(argv[0]);
+        return 0;
+    }
 
-    PrintHelp(argv[0]);
+    std::string input;
+    std::string output;
+
+    std::string mode;
+    bool customSBoxes = false;
+
+    long threads = -1;
+    long threadSize = -1;
+
+    for (int i=1; i<argc; i++)
+    {
+        if (std::string(argv[i]) == "--help")
+        {
+            PrintHelp(argv[0]);
+            return 0;
+        }
+    }
+
+    std::vector <bool> usedArgs;
+    usedArgs.reserve(argc);
+    usedArgs.push_back(true);
+    for (int i=1; i<argc; i++)
+    {
+        usedArgs.push_back(false);
+    }
+
+    //catch custom sboxes option
+    for (int i=1; i<argc; i++)
+    {
+        if ((std::string(argv[i]) == "--custom-s-boxes") ||
+            (std::string(argv[i]) == "-b")               )
+        {
+            if (customSBoxes)
+            {
+                PrintHelp(argv[0]);
+                return 0;
+            } // for the repeated action used
+
+            customSBoxes = true;
+            usedArgs.at(i) = true;
+        }
+    }
+
+    //catch mode of operation
+    for (int i=1; i<argc; i++)
+    {
+        if (usedArgs.at(i))
+        {
+            continue;
+        }
+
+        if ((std::string(argv[i]) == "--mode-of-operation") ||
+            (std::string(argv[i]) == "-m")                  )
+        {
+            if (0 !=mode.size())
+            {
+                PrintHelp(argv[0]);
+                return 0;
+            } // for the repeated action used
+
+            if ((i+1 < argc)                &&
+                (argv[i+1][0] != '-')       &&
+                (false == usedArgs.at(i+1)) )
+            {
+                mode = argv[i+1];
+                usedArgs.at(i) = true;
+                usedArgs.at(i+1) = true;
+            }
+            else
+            {
+                PrintHelp(argv[0]);
+                return 0;
+            }
+        }
+    }
+
+
+    //catch threds count
+    for (int i=1; i<argc; i++)
+    {
+        if (usedArgs.at(i))
+        {
+            continue;
+        }
+
+        if ((std::string(argv[i]) == "--threads-count") ||
+            (std::string(argv[i]) == "-t")              )
+        {
+            if (-1 !=threads)
+            {
+                PrintHelp(argv[0]);
+                return 0;
+            } // for the repeated action used
+
+            if ((i+1 < argc)                &&
+                (argv[i+1][0] != '-')       &&
+                (false == usedArgs.at(i+1)) )
+            {
+                char * pEnd;
+                threads = strtol(argv[i+1], &pEnd, 10);
+                if (0L == threads)
+                {
+                    PrintHelp(argv[0]);
+                    return 0;
+                }
+                usedArgs.at(i) = true;
+                usedArgs.at(i+1) = true;
+            }
+            else
+            {
+                PrintHelp(argv[0]);
+                return 0;
+            }
+        }
+    }
+
+    //catch thread size
+    for (int i=1; i<argc; i++)
+    {
+        if (usedArgs.at(i))
+        {
+            continue;
+        }
+
+        if ((std::string(argv[i]) == "--thread-size") ||
+            (std::string(argv[i]) == "-s")            )
+        {
+            if (-1 !=threadSize)
+            {
+                PrintHelp(argv[0]);
+                return 0;
+            } // for the repeated action used
+
+            if ((i+1 < argc)                &&
+                (argv[i+1][0] != '-')       &&
+                (false == usedArgs.at(i+1)) )
+            {
+                char * pEnd;
+                threadSize = strtol(argv[i+1], &pEnd, 10);
+                if (0L == threadSize)
+                {
+                    PrintHelp(argv[0]);
+                    return 0;
+                }
+                usedArgs.at(i) = true;
+                usedArgs.at(i+1) = true;
+            }
+            else
+            {
+                PrintHelp(argv[0]);
+                return 0;
+            }
+        }
+    }
+
+    //at this moment only input string or input/output file
+    size_t inputIndex = 0;
+    size_t outputIndex = 0;
+
+    for (size_t i=0; i<usedArgs.size(); i++)
+    {
+        if (false == usedArgs.at(i))
+        {
+            if (0 == inputIndex)
+            {
+                inputIndex = i;
+            }
+            else if (0 == outputIndex)
+            {
+                outputIndex = i;
+            }
+            else
+            {
+                PrintHelp(argv[0]);
+                return 0;
+            }
+        }
+    }
+
+    bool fileEncoding = false;
+
+    if (0 == inputIndex)
+    {
+        PrintHelp(argv[0]);
+        return 0;
+    }
+    else
+    {
+        input = argv[inputIndex];
+        if (0 != outputIndex)
+        {
+            output = argv[outputIndex];
+            fileEncoding = true;
+        }
+    }
 
     return 0;
 }
